3a7e6d39ba7820b50a06ad30146bcd1420cc99e4
Rodrigo A. Arvolea <arvolea@hotmail.com>
2015-03-08 18:28:14 -0300

    Course Project 1 Files
    Submit files to my GitHub repository containing the completed R code and
    graphs for this assignment.

---
+++ Plot2.R
@@ -0,0 +1,11 @@
+#Coursera - Exploratory Data Analysis
+#setwd("C:\A5-Learning\Coursera\Data Science\4-Exploratory Data Analysis\Course Project 1")
+#Course Project 1 - Plot 1
+dataSource <- "./household_power_consumption.txt"
+data <- read.table(dataSource, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
